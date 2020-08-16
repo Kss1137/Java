d0d30520ce35c4180aefb016dfa055b6373b2497
Kss1137 <59612423+Kss1137@users.noreply.github.com>
2020-08-16 17:45:25 +0900

    Add files via upload
    Study +-*/ and replace, length, \, \"", int, double, Sting, "~~" = string, 'H' = Character

---
+++ PracticeApp.java
@@ -0,0 +1,28 @@
+
+public class PracticeApp {
+
+	public static void main(String[] args) {
+	    System.out.println(Math.PI);
+	    System.out.println(6 * 5);
+	    System.out.println(4 / 2);
+	    System.out.println(5 - 3);
+		int a = 4;
+	    double b = 7.2;
+	    double c = 4.5;
+	    System.out.println(Math.floor(c));
+	    System.out.println(Math.ceil(b));
+	    System.out.println(6 + a);
+	    System.out.println("으아아아 아아아아"   
+	    		+ "dkdkdkdkd" .length()); 
+	    System.out.println("아아아아아 \"아아아아아앙\""); //escape
+	    System.out.println("아아아아아"); //string 열
+	    System.out.println('h'); //Character 문자
+	    System.out.println("앙ㅇㅇ아아아아 /아아아아아아아ㅏ");	    
+	    String o = "살려줍쇼";
+	    System.out.println("오오"); 
+	    System.out.println(o);
+	    System.out.println("bye bye bye" .replace("bye", "newmachine"));
+	    System.out.println();
+	}
+
+}
